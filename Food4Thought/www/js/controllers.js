56d05f9fdef2a73c3f8157abd720a709d8ebdef2
rachellewis <rachlewis0000@gmail.com>
2017-04-08 14:42:40 -0600

    new admin notifications page

--- Food4Thought/www/js/controllers.js
+++ Food4Thought/www/js/controllers.js
@@ -12,7 +12,7 @@ angular.module('starter.controllers', [])
 	});
 	var vm = this;
 	vm.storeCredentials = storeCredentials;	
-	
+
 	console.log($scope.$storage.firstTime);
 	if($scope.$storage.firstTime == false){
 		$state.go('tab.home');
@@ -21,7 +21,7 @@ angular.module('starter.controllers', [])
 	function storeCredentials(){
 		if(vm.welcomeForm.$valid){
 			$scope.$storage.firstTime = false;	
-			
+
 			$scope.$storage.users[$scope.userData.email] = {
 				firstName: $scope.userData.firstName,
 				lastName: $scope.userData.lastName,
@@ -73,16 +73,16 @@ angular.module('starter.controllers', [])
 			}
 		}
 	}
-				//User is in list of users. Set them as active user and navigate to home page
+	//User is in list of users. Set them as active user and navigate to home page
 
-			//look for $scope.userData.email in $scope.$storage.users
-	
+	//look for $scope.userData.email in $scope.$storage.users
+
 
 
 
 })
 .controller('HomeCtrl', function($state, $scope, $ionicModal, $localStorage) {
-	
+
 	$scope.$storage = $localStorage;
 	console.log($scope.$storage.currentUser);
 
@@ -92,7 +92,7 @@ angular.module('starter.controllers', [])
 	];
 
 	$scope.locDetails = [
-		{name: 'MSU', info: '7:30-9:30am\nThe Regency Athletic Complex at MSU Denver. 1390 Shoshone St, Denver, CO 80204'}
+	{name: 'MSU', info: '7:30-9:30am\nThe Regency Athletic Complex at MSU Denver. 1390 Shoshone St, Denver, CO 80204'}
 	];
 
 	$scope.clothInfo = "Please wear comforable clothing and dress for working in an outdoor covered location.";
@@ -181,29 +181,152 @@ angular.module('starter.controllers', [])
 })
 
 
-.controller('NotificationsCtrl', function($scope) {
-	$scope.notificationsUrgent = [
-	{ title: "snowman apocalypse" },
-	{ title: "blah" }
-	];
+.controller('NotificationsCtrl', function(UrgentFactory, InfoFactory, $scope, $ionicModal) {
+	// load the add/change dialog from the given template url
+	$ionicModal.fromTemplateUrl('templates/admin/urgent-dialog.html', function(modal) {
+		$scope.addUrgentDialog = modal;
+	},  {
+		scope: $scope,
+		animation: 'slide-in-up'
+	});
+	$ionicModal.fromTemplateUrl('templates/admin/info-dialog.html', function(modal) {
+		$scope.addInfoDialog = modal;
+	}, {
+		scope: $scope,
+		animation: 'slide-in-up'
+	});
 
-	$scope.notificationsInfo = [
-	{title: "something cool and informative, yay kids!"}
-	];
+	$scope.showAddChangeUrgent = function(action) {
+		$scope.urgentAction = action;
+		$scope.addUrgentDialog.show();
+	};
+	$scope.showAddChangeInfo = function(action) {
+		$scope.infoAction = action;
+		$scope.addInfoDialog.show();
+	};
 
-	$scope.createUrgent = function(notification) {
+	$scope.leaveAddChangeUrgent = function() {
+		// remove dialog
+		$scope.addUrgentDialog.remove();
+		// reload modal template to have cleared form
+		$ionicModal.fromTemplateUrl('templates/admin/urgent-dialog.html', function(modal) {
+			$scope.addUrgentDialog = modal;
+		}, {
+			scope: $scope,
+			animation: 'slide-in-up'
+		});
+	};
+	$scope.leaveAddChangeInfo = function() {
+		//remove dialog
+		$scope.addInfoDialog.remove();
+		//reload modal template to have cleared form
+		$ionicModal.fromTemplateUrl('templates/admin/info-dialog.html', function(modal) {
+			$scope.addInfoDialog = modal;
+		}, {
+			scope: $scope,
+			animation: 'slide-in-up'
+		});
+	};
+
+	$scope.urgent = UrgentFactory.getList();
+	$scope.info = InfoFactory.getList();
+
+	$scope.saveEmpty = function(form) {
+		$scope.form = angular.copy(form);
+	};
+
+	$scope.addUrgent = function(form) {
+		var newItem = {};
+		// add values from form to object
+		newItem.description = form.description.$modelValue;
+		// save new list in scope and factory
+		$scope.urgent.push(newItem);
+		UrgentFactory.setList($scope.urgent);
+		// close dialog
+		$scope.leaveAddChangeUrgent();
+	};
+	$scope.addInfo = function(form) {
+		var newItem = {};
+		// add values from form to object
+		newItem.description = form.description.$modelValue;
+		// save new list in scope and factory
+		$scope.info.push(newItem);
+		InfoFactory.setList($scope.info);
+		// close dialog
+		$scope.leaveAddChangeInfo();
+	};
+
+	$scope.removeUrgent = function(item) {
+		// search & destroy item from list
+		$scope.urgent.splice($scope.urgent.indexOf(item), 1);
+		// if this item was the default we set first item in list to default
+		// save list in factory
+		UrgentFactory.setList($scope.urgent);
+	};
+	$scope.removeInfo = function(item) {
+		// search & destroy item from list
+		$scope.info.splice($scope.info.indexOf(item), 1);
+		// if this item was the default we set first item in list to default
+		// save list in factory
+		InfoFactory.setList($scope.info);
+	};
+
+	$scope.showEditUrgent = function(item) {
+		// remember edit item to change it later
+		$scope.tmpEditUrgent = item;
+
+		// preset form values
+		$scope.form.description.$setViewValue(item.description);
+		// open dialog
+		$scope.showAddChangeUrgent('change');
+	};
+	$scope.showEditInfo = function(item) {
+		$scope.tmpEditInfo = item;
+
+		$scope.form.description.$setViewValue(item.description);
+		$scope.showAddChangeInfo('change');
+	};
+
+	$scope.editUrgent = function(form) {
+		var item = {};
+		item.description = form.description.$modelValue;
+
+		var editIndex = UrgentFactory.getList().indexOf($scope.tmpEditUrgent);
+		$scope.urgent[editIndex] = item;
+		// set first item to default
+		UrgentFactory.setList($scope.urgent);
+		$scope.leaveAddChangeUrgent();
+	};
+	$scope.editInfo = function(form) {
+		var item = {};
+		item.description = form.description.$modelValue;
+
+		var editIndex = InfoFactory.getList().indexOf($scope.tmpEditInfo);
+		$scope.info[editIndex] = item;
+		// set first item to default 
+		InfoFactory.setList($scope.info);
+		$scope.leaveAddChangeInfo();
+	};      
+
+	/*	$scope.notificationsUrgent = [
+		{ title: "snowman apocalypse" },
+		{ title: "blah" }
+		];
+		$scope.notificationsInfo = [
+		{title: "something cool and informative, yay kids!"}
+		];
+		$scope.createUrgent = function(notification) {
 		$scope.notificationsUrgent.push({
-			title: notification.title
+		title: notification.title
 		});
 		notification.title = "";
-	};
-	$scope.createInfo = function(notification) {
+		};
+		$scope.createInfo = function(notification) {
 		$scope.notificationsInfo.push({
-			title: notification.title
+		title: notification.title
 		});
 		notification.title = "";
-	};
-
+		};*/
 })
 
 .controller('Admin-NotCtrl', function($scope, $ionicModal) {
@@ -227,7 +350,45 @@ angular.module('starter.controllers', [])
 		$scope.checkInModal = modal;
 	});
 
-$scope.openModal = function(modalID) {
+	$scope.openModal = function(modalID) {
+		if(modalID == 1) $scope.signUpModal.show();
+		else $scope.checkInModal.show();
+		//$scope.checkInModal.show();
+	};
+	$scope.closeModal = function() {
+		if(modalID == 1) $scope.signUpModal.hide();
+		else $scope.checkInModal.hide();
+	};
+
+	// Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.signUpModal.remove();
+		$scope.checkInModal.remove();
+	});
+})
+
+.controller('Admin-NotCtrl', function($scope, $ionicModal) {
+
+	// Edit Notification Modal
+	$ionicModal.fromTemplateUrl('templates/admin/modals/editNotification.html', {
+		id: '1',
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.signUpModal = modal;
+	});
+
+
+	// New Notification Modal
+	$ionicModal.fromTemplateUrl('templates/admin/modals/newNotification.html', {
+		id: '2',
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.checkInModal = modal;
+	});
+
+	$scope.openModal = function(modalID) {
 		if(modalID == 1) $scope.signUpModal.show();
 		else $scope.checkInModal.show();
 		//$scope.checkInModal.show();
@@ -282,13 +443,13 @@ $scope.openModal = function(modalID) {
 })
 
 .controller('CheckinCtrl', function($scope, $state, $ionicFilterBar, $timeout) {
-	
+
 	var filterBarInstance;
 
 	var currentShiftIndex;
 	var currentUncheckedVols = [];
 	var currentCheckedVols = [[],[]];
-    function getItems () {
+	function getItems () {
 
 		//	Hide shift selector when there's only one shift for the current day
 
@@ -321,12 +482,12 @@ $scope.openModal = function(modalID) {
 			opt.innerHTML = shiftNames[i]; 
 			shiftSelector.appendChild(opt);
 		}
-		
+
 		document.getElementById("shiftSelector").appendChild(shiftSelector);
 		var uncheckedVolunteers;
 		var checkedVolunteers;
 		//This is where we'll put the call to get the volunteer lists for shifts occuring 'today'
-	
+
 		for(var i = 0; i < currentUncheckedVols.length; i++){
 			for(var j = 0; j < currentUncheckedVols[i].length; j++){
 				currentUncheckedVols[i][j] = ({text: currentUncheckedVols[i][j], selected: false, deleted: false});
@@ -349,46 +510,46 @@ $scope.openModal = function(modalID) {
 		}
 	}; 
 
-    getItems();
+	getItems();
 
-    $scope.showFilterBar = function () {
-      filterBarInstance = $ionicFilterBar.show({
-        items: $scope.uncheckedVolunteers,
-        update: function (filteredItems, filterText) {
-          $scope.uncheckedVolunteers = filteredItems;
-		},
+	$scope.showFilterBar = function () {
+		filterBarInstance = $ionicFilterBar.show({
+			items: $scope.uncheckedVolunteers,
+			update: function (filteredItems, filterText) {
+				$scope.uncheckedVolunteers = filteredItems;
+			},
 
-		done: function(){
-			$scope.hide = "hidden";
-		},
+			done: function(){
+				$scope.hide = "hidden";
+			},
 
-		cancel: function (){
-			$scope.hide = "";
-			for(var i = $scope.uncheckedVolunteers.length-1; i >= 0; i--){
-				if($scope.uncheckedVolunteers[i].deleted){
-					$scope.uncheckedVolunteers.splice(i, 1);
+			cancel: function (){
+				$scope.hide = "";
+				for(var i = $scope.uncheckedVolunteers.length-1; i >= 0; i--){
+					if($scope.uncheckedVolunteers[i].deleted){
+						$scope.uncheckedVolunteers.splice(i, 1);
+					}
 				}
 			}
+		});
+	};
+
+	$scope.refreshItems = function () {
+		if (filterBarInstance) {
+			filterBarInstance();
+			filterBarInstance = null;
+
 		}
-      });
-    };
 
-    $scope.refreshItems = function () {
-      if (filterBarInstance) {
-        filterBarInstance();
-        filterBarInstance = null;
-		
-      }
-
-      $timeout(function () {
-        getItems();
-        $scope.$broadcast('scroll.refreshComplete');
-      }, 1000);
-    };
+		$timeout(function () {
+			getItems();
+			$scope.$broadcast('scroll.refreshComplete');
+		}, 1000);
+	};
 })
 
 .controller('AdminCtrl', function($scope, $state) {
-	 
+
 })
 
 .controller('SettingsCtrl', function($scope) {
